88f64e87fbf2b18426f558f8c429e4c4f078d473
meheraj786 <meherajhosen786@gmail.com>
2026-07-21 20:31:20 +0600

    minor

--- src/modules/supplier/supplier.validation.ts
+++ src/modules/supplier/supplier.validation.ts
@@ -1,26 +1,26 @@
 import { z } from "zod";
 
 const createSupplierZodSchema = z.object({
-	body: z.object({
-		name: z.string().min(1, "Name is required"),
-		contact: z.string().min(1, "Contact is required"),
-		email: z.string().email("Invalid email").optional().or(z.literal("")),
-		companyName: z.string().optional().or(z.literal("")),
-		address: z.string().optional().or(z.literal("")),
-	}),
+  body: z.object({
+    name: z.string().min(1, "Name is required"),
+    contact: z.string().min(1, "Contact is required"),
+    email: z.string().email("Invalid email").optional().or(z.literal("")),
+    companyName: z.string().optional().or(z.literal("")),
+    address: z.string().optional().or(z.literal("")),
+  }),
 });
 
 const updateSupplierZodSchema = z.object({
-	body: z.object({
-		name: z.string().min(1).optional(),
-		contact: z.string().min(1).optional(),
-		email: z.string().email("Invalid email").optional().or(z.literal("")),
-		companyName: z.string().optional().or(z.literal("")),
-		address: z.string().optional().or(z.literal("")),
-	}),
+  body: z.object({
+    name: z.string().min(1).optional(),
+    contact: z.string().min(1).optional(),
+    email: z.string().email("Invalid email").optional().or(z.literal("")),
+    companyName: z.string().optional().or(z.literal("")),
+    address: z.string().optional().or(z.literal("")),
+  }),
 });
 
 export const SupplierValidation = {
-	createSupplierZodSchema,
-	updateSupplierZodSchema,
+  createSupplierZodSchema,
+  updateSupplierZodSchema,
 };
